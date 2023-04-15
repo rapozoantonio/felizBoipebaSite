a8c69f3ed6c4997d9a8dcd2c034be8f1881672cf
Antonio Rapozo <antonio.rapozo@mayvue.com>
2023-04-15 10:23:10 -0600

    remove console errorsr

--- js/main.js
+++ js/main.js
@@ -9,24 +9,24 @@
 		closebtn = document.getElementById( 'close-button' ),
 		isOpen = false;
 
-	function init() {
-		initEvents();
-	}
+	// function init() {
+	// 	initEvents();
+	// }
 
-	function initEvents() {
-		openbtn.addEventListener( 'click', toggleMenu );
-		if( closebtn ) {
-			closebtn.addEventListener( 'click', toggleMenu );
-		}
+	// function initEvents() {
+	// 	openbtn.addEventListener( 'click', toggleMenu );
+	// 	if( closebtn ) {
+	// 		closebtn.addEventListener( 'click', toggleMenu );
+	// 	}
 
-		// close the menu element if the target it´s not the menu element or one of its descendants..
-		content.addEventListener( 'click', function(ev) {
-			var target = ev.target;
-			if( isOpen && target !== openbtn ) {
-				toggleMenu();
-			}
-		} );
-	}
+	// 	// close the menu element if the target it´s not the menu element or one of its descendants..
+	// 	content.addEventListener( 'click', function(ev) {
+	// 		var target = ev.target;
+	// 		if( isOpen && target !== openbtn ) {
+	// 			toggleMenu();
+	// 		}
+	// 	} );
+	// }
 
 	function toggleMenu() {
 		if( isOpen ) {
@@ -38,6 +38,6 @@
 		isOpen = !isOpen;
 	}
 
-	init();
+	// init();
 
 })();
